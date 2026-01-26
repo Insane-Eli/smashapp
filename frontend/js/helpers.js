aa90f75bfbdeb06ae518a6b669760a50b005dadd
Insane-Eli <eli@currah.com>
2026-01-26 11:20:05 -0800

    more restructuring + hopefully css works now

--- frontend/js/helpers.js
+++
@@ -1,8 +0,0 @@
-
-function jsonify(response){
-response
-    .then((res) => res.json())
-    .then((data) => JSON.stringify(data, null, 2))
-    .catch((err) => console.error(err));
-}
-return response;
