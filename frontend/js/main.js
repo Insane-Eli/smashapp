73efffbe58b65f9b11377299e0e1bce798fa2edf
Insane-Eli <eli@currah.com>
2026-01-26 15:57:16 -0800

    trying to connect user input with backend functions

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,13 +1,17 @@
+import {fetchTournamentInfo} from '../../backend/fetch-tournament-data';
+
 const input = document.getElementById("playerTag");
 
 input.addEventListener("keydown", function(e) {
   if (e.key === "Enter") {
-    getPlayer();
+    getTournamentInfo();
   }
 });
 
-function getPlayer(){
+async function getTournamentInfo() {
   const output = document.getElementById("output");
   const input = document.getElementById("playerTag").value;
-  output.textContent = input;
+  const data = await fetchTournamentInfo(input); // wait for API
+  output.textContent = JSON.stringify(data, null, 2); // nicely formatted
 }
+
