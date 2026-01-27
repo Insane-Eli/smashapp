74faebf39e0e393b3982011092248d6b9d9313ae
Insane-Eli <eli@currah.com>
2026-01-27 07:40:02 -0800

    package.json, server.js, and main.js all updated + formatting
    its looking realer every day ;u;

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,19 @@
 const input = document.getElementById("playerTag");
+const output = document.getElementById("output");
+const button = document.getElementById("search");
+
+
+async function getTournamentInfo() {
+  const slug = input.value.trim();
+  if (!slug) return;
+
+  output.textContent = "loading..";
+
+  const res = await fetch(`http://localhost:3000/api/tournament?slug=${slug}`);
+  const data = await res.json();
+
+  output.textContent = JSON.stringify(data, null, 2);
+}
 
 input.addEventListener("keydown", function(e) {
   if (e.key === "Enter") {
@@ -6,10 +21,4 @@ input.addEventListener("keydown", function(e) {
   }
 });
 
-async function getTournamentInfo() {
-  const output = document.getElementById("output");
-  const input = document.getElementById("playerTag").value;
-  const data = await fetchTournamentInfo(input);
-  output.textContent = data;
-}
-
+button.addEventListener("click", getTournamentInfo);
