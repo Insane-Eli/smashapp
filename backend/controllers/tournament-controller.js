3853d3bd4aab7d4781109ae5bb9cc6d61b82d25f
Eli <eli@currah.com>
2026-05-02 21:31:50 -0700

    experimented with API queries

--- backend/controllers/tournament-controller.js
+++ backend/controllers/tournament-controller.js
@@ -2,10 +2,9 @@ import fs from "fs";
 const API_KEY = fs.readFileSync(".env", "utf8").trim();
 
 export async function getTournament(req, res) {
-
   const { slug } = req.query;
 
-  try{
+  try {
     const response = await fetch("https://api.start.gg/gql/alpha", {
       method: "POST",
       headers: {
@@ -16,25 +15,54 @@ export async function getTournament(req, res) {
         query: `
           query {
             tournament(slug: "tournament/${slug}") {
-              name
-              startAt
-              state
+              player{
+                character
+              }
             }
           }
         `,
       }),
     });
 
-  if (!response.ok) {
-    const text = await response.text();
-    console.error("start.gg api error: ", text);
-    return res.status(response.status).json({ error: "start.gg api error", details: text });
+    /*
+    query TournamentQuery($slug: String!, $page: Int!) {
+  tournament(slug: $slug) {
+    name
+    events {
+      name
+      numEntrants
+      sets(page: $page, perPage: 10) {
+        nodes {
+          games {
+            selections {
+              entrant {
+                id
+                name
+              }
+              selectionValue
+            }
+          }
+        }
+        pageInfo {
+          totalPages
+        }
+      }
+    }
   }
+}
+  */
 
-  const data = await response.json();
-  console.log(JSON.stringify(data, null, 2));
-  res.json(data);
+    if (!response.ok) {
+      const text = await response.text();
+      console.error("start.gg api error: ", text);
+      return res
+        .status(response.status)
+        .json({ error: "start.gg api error", details: text });
+    }
 
+    const data = await response.json();
+    console.log(JSON.stringify(data, null, 2));
+    res.json(data);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "fetch failed" });
